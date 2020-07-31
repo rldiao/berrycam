e8773c5ffb33c1d757338ea1cf9b84c65e55273c
Robert Diao <43899833+rldiao@users.noreply.github.com>
2020-07-31 16:38:43 +1000

    feat: resizable main window

--- berrycam/app.py
+++ berrycam/app.py
@@ -11,6 +11,7 @@ if __name__ == '__main__':
     logger.info('Running BerryCam...')
     with camera_provider():
         root_window = tk.Tk()
+        root_window.resizable(height=None, width=None)
         index = Index(master=root_window)
         index.mainloop()
 
