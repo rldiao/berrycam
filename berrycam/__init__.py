c635a1a78a8ae6ca68087adf5546057382e44c5a
Robert Diao <43899833+rldiao@users.noreply.github.com>
2020-08-03 18:46:58 +1000

    clean exit function to close camera

--- berrycam/__init__.py
+++ berrycam/__init__.py
@@ -1,4 +1,6 @@
+import atexit
 from flask import Flask
+from berrycam.camera import camera_provider
 
 import logging
 from logging.config import dictConfig
@@ -27,9 +29,19 @@ logging_config = dict(
 dictConfig(logging_config)
 
 
+def clean_exit(logger, *args, **kwargs):
+    camera_provider().close()
+    print('')
+    logger.info('Camera Status - {}'.format(not camera_provider().closed))
+
+
 def create_app():
     app = Flask(__name__, static_folder='./build', static_url_path='/', instance_relative_config=False)
     app.config.from_object('config.DevConfig')
+
+    app.logger.info('Running BerryCam')
+    camera = camera_provider()
+    app.logger.info('Camera Status - {}'.format(not camera.closed))
 
     with app.app_context():
         # Import application components
@@ -37,5 +49,7 @@ def create_app():
 
         # Register blueprints
         app.register_blueprint(camera_api_bp)
+
+    atexit.register(clean_exit, app.logger)
 
     return app
--- berrycam/run.py
+++ berrycam/run.py
@@ -1,5 +1,5 @@
 from berrycam import create_app
-from berrycam.camera import camera_provider
+
 
 app = create_app()
 
@@ -7,10 +7,3 @@ app = create_app()
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
-
-
-app.logger.info('Running BerryCam...')
-camera_provider()
-app.logger.debug('Camera Status - {}'.format(not camera_provider().closed))
-camera_provider().close()
-app.logger.debug('Camera Status - {}'.format(not camera_provider().closed))
